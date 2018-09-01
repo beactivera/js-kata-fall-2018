8fa9ea47644a1ac2ab6d7b01af25a690f1f675ef
Weronika Wdowiak <weronika.wdowiak56@gmail.com>
2018-09-01 14:58:05 +0200

    found why it was not working and found another solution

--- w1d2 - this site uses cookies/mysolution/main.js
+++ w1d2 - this site uses cookies/mysolution/main.js
@@ -1,7 +1,36 @@
-let not = document.querySelector(".cookies");
-// window.onload = showNot();
+// let not = document.querySelector(".cookies");
+// let cross = document.querySelector(".cross");
+// window.addEventListener("load", popUpCookie);
 
-// function showNot(){
-//     not.addEventListener("animationstart");
-
+// function popUpCookie() {
+//   not.classList.add("slide-up");
 // }
+// cross.addEventListener("click", closeNot);
+
+// function closeNot() {
+//   not.classList.remove("slide-up");
+//   not.classList.add("slide-down");
+// }
+
+// second solution
+
+window.addEventListener("load", () => {
+  const not = document.querySelector("#cookies");
+  const accept = not.querySelector(".accept");
+  const deny = not.querySelector(".deny");
+
+  not.classList.add("moveIn");
+
+  hookUpEvents();
+  function hookUpEvents() {
+    accept.addEventListener("click", () => {
+      not.classList.remove("moveIn");
+    });
+    deny.addEventListener("click", () => {
+      not.classList.remove("moveIn");
+    });
+    deny.addEventListener("mouseenter", () => {
+      deny.style.transform = `translateY(${Math.random() * 300})`;
+    });
+  }
+});
