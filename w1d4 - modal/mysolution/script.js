2127c2b1228f5163ddae90b9325e08956ce2ce1c
Weronika Wdowiak <weronika.wdowiak56@gmail.com>
2018-09-01 17:31:33 +0200

    first approch finding the solution

--- w1d4 - modal/mysolution/script.js
+++ w1d4 - modal/mysolution/script.js
@@ -1,9 +1,29 @@
 "use strict";
+const modal = document.querySelector("#modal_container");
+const closeBtn = document.querySelector("#closebutton");
 
-function showModal() {
-  console.log("Show modal");
-}
+const pdk1 = document.querySelector("#product_1");
+const pdk2 = document.querySelector("#product_2");
+const pdk3 = document.querySelector("#product_3");
+const pdk4 = document.querySelector("#product_4");
+const pdk5 = document.querySelector("#product_5");
 
-function hideModal() {
-  console.log("Hide modal");
-}
+const products = [pdk1, pdk2, pdk3, pdk4, pdk5];
+
+products.forEach(element => {
+  for (let i = 0; i < products.length; i++) {
+    let details = products[i].lastChild;
+    details.addEventListener("click", showModal);
+
+    function showModal() {
+      console.log("Show modal");
+      modal.classList.remove("hide");
+      closeBtn.addEventListener("click", hideModal);
+
+      function hideModal() {
+        console.log("Hide modal");
+        modal.classList.add("hide");
+      }
+    }
+  }
+});
