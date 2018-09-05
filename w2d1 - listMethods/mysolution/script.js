d161c836064cc9f0c760c0e2439a251bd1aaff37
Weronika Wdowiak <weronika.wdowiak56@gmail.com>
2018-09-05 08:59:01 +0200

    first approach to solve the exercise

--- w2d1 - listMethods/mysolution/script.js
+++ w2d1 - listMethods/mysolution/script.js
@@ -1,16 +1,26 @@
 "use strict";
+let sections;
+let uls;
 
 function displayElement(element) {
   console.log(element.textContent);
 }
 
-// listMethods("modify");
-// listMethods("info");
-// listMethods("newarray");
+listMethods("modify");
+listMethods("info");
+listMethods("newarray");
+
+sections = document.querySelectorAll("section");
+console.log(sections);
 
 // TODO: Create listMethods function
-// function listMethods(){
+function listMethods() {
+  for (let i = 0; i < sections.length; i++) {
+    uls = sections[i].lastElementChild;
+    console.log(uls);
+  }
 
-// }
-
-console.log(document.querySelectorAll("#id"));
+  // for (let i = 0; i < arrElement.length; i++) {
+  //   console.log(arrElement[i].lastElementChild);
+  // }
+}
